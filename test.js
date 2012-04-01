efffc3c9c96f7cfdbddb2b79382fa3b5efd85dfc
neekey <ni184775761@gmail.com>
2012-04-01 16:43:25 +0800

    init

---
+++ test.js
@@ -0,0 +1,106 @@
+var QueueMethod = require( './index').QueueMethod;
+
+var sleep = QueueMethod( function ( s, fn ){
+
+    setTimeout( fn, s );
+});
+
+var sleep2 = QueueMethod( function ( s, fn ){
+
+    setTimeout( fn, s );
+});
+
+
+var sleep3 = QueueMethod( function ( next ){
+
+    sleep( 2000, function (){
+
+        next();
+    });
+});
+
+var sleep4 = QueueMethod( function ( next ){
+
+    sleep( 2000, function (){
+
+        console.log( 'sleep 4 1');
+//        next();
+        sleep5(function (){
+            console.log( 'sleep 4 1 1');
+        });
+    });
+
+    sleep( 2000, function (){
+
+        console.log( 'sleep 4 2');
+
+
+        next();
+    });
+});
+
+var sleep5 = QueueMethod( function ( next ){
+
+    sleep( 2000, function (){
+
+        console.log( 'sleep 5 1');
+    });
+
+    sleep( 2000, function (){
+
+        console.log( 'sleep 5 2');
+
+
+        next();
+    });
+});
+
+
+console.log( sleep3.toString() );
+
+
+sleep( 5000, function (){
+
+    console.log( 'sleep 1' );
+
+    sleep2( 3000, function (){
+
+        console.log( 'sleep 1.1' );
+    });
+
+    sleep3(function (){
+        console.log( 'sleep 1.2');
+    });
+
+    sleep4(function (){
+        console.log( 'sleep 1.2.1');
+    });
+
+    sleep2( 2000, function (){
+
+        console.log( 'sleep 1.3' );
+
+        sleep( 2000, function (){
+
+            console.log( 'sleep 1.3.1' );
+        });
+    });
+});
+
+sleep3(function (){
+    console.log( 'sleep 2')
+});
+
+sleep( 1000, function (){
+    console.log( 'sleep 3');
+
+    sleep2( 2000, function (){
+
+        console.log( 'sleep 3.1' );
+    });
+});
+
+sleep( 1000, function (){
+    console.log( 'sleep 4');
+});
+
